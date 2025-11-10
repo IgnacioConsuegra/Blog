dc8ac36ef2e0f833f4846806bbfcd7f671d8a086
Ignacio Consuegra <ignacioandresconsuegradelacruz@gmail.com>
2025-11-10 07:00:06 -0500

    Try this right know123.

--- frontend/src/Utils/base_Url.js
+++ frontend/src/Utils/base_Url.js
@@ -1,2 +1,4 @@
 export const BASE_URL =
-  import.meta.env.MODE === "development" ? "http://localhost:5001/" : "/";
+  import.meta.env.MODE === "development"
+    ? "http://localhost:5001"
+    : "https://blog-evtq.onrender.com";
